2583a0d450466c963f8c834587a2e17e20dfdf85
dchan3 <dchan3@hawaii.edu>
2018-04-27 16:43:26 -1000

    Change non-equality operators to XOR where possible

--- index.js
+++ index.js
@@ -14,36 +14,33 @@ module.exports = function equal(a, b) {
       , length
       , key;
 
-    if (arrA && arrB) {
-      length = a.length;
-      if (length != b.length) return false;
-      for (i = length; i-- !== 0;)
-        if (!equal(a[i], b[i])) return false;
-      return true;
-    }
+  if (arrA ^ arrB) return false;
+  if (arrA && arrB) {
+    length = a.length;
+    if (length ^ b.length) return false;
+    for (i = length; i-- ^ 0;)
+      if (!equal(a[i], b[i])) return false;
+    return true;
+  }
 
-    if (arrA != arrB) return false;
+  var dateA = a instanceof Date
+    , dateB = b instanceof Date;
+  if (dateA ^ dateB) return false;
+  if (dateA && dateB) return a.getTime() == b.getTime();
 
-    var dateA = a instanceof Date
-      , dateB = b instanceof Date;
-    if (dateA != dateB) return false;
-    if (dateA && dateB) return a.getTime() == b.getTime();
-
-    var regexpA = a instanceof RegExp
-      , regexpB = b instanceof RegExp;
-    if (regexpA != regexpB) return false;
-    if (regexpA && regexpB) return a.toString() == b.toString();
+  var regexpA = a instanceof RegExp
+    , regexpB = b instanceof RegExp;
+  if (regexpA ^ regexpB) return false;
+  if (regexpA && regexpB) return a.toString() == b.toString();
 
     var keys = keyList(a);
     length = keys.length;
 
-    if (length !== keyList(b).length)
-      return false;
+    if (length ^ keyList(b).length) return false;
 
-    for (i = length; i-- !== 0;)
-      if (!hasProp.call(b, keys[i])) return false;
+    for (i = length; i-- ^ 0;) if (!hasProp.call(b, keys[i])) return false;
 
-    for (i = length; i-- !== 0;) {
+    for (i = length; i-- ^ 0;) {
       key = keys[i];
       if (!equal(a[key], b[key])) return false;
     }
