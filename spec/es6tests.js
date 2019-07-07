ebb0eff2e0ca69ab4b8957b464b275b3308aa00d
Evgeny Poberezkin <e.poberezkin@me.com>
2019-07-07 13:47:50 +0100

    support es6 typed arrays, #32

--- spec/es6tests.js
+++ spec/es6tests.js
@@ -207,154 +207,54 @@ module.exports = [
         equal: false
       }
     ]
+  },
 
-  // {
-  //   description: 'arrays',
-  //   tests: [
-  //     {
-  //       description: 'two empty arrays are equal',
-  //       value1: [],
-  //       value2: [],
-  //       equal: true
-  //     },
-  //     {
-  //       description: 'equal arrays',
-  //       value1: [1, 2, 3],
-  //       value2: [1, 2, 3],
-  //       equal: true
-  //     },
-  //     {
-  //       description: 'not equal arrays (different item)',
-  //       value1: [1, 2, 3],
-  //       value2: [1, 2, 4],
-  //       equal: false
-  //     },
-  //     {
-  //       description: 'not equal arrays (different length)',
-  //       value1: [1, 2, 3],
-  //       value2: [1, 2],
-  //       equal: false
-  //     },
-  //     {
-  //       description: 'equal arrays of objects',
-  //       value1: [{a: 'a'}, {b: 'b'}],
-  //       value2: [{a: 'a'}, {b: 'b'}],
-  //       equal: true
-  //     },
-  //     {
-  //       description: 'not equal arrays of objects',
-  //       value1: [{a: 'a'}, {b: 'b'}],
-  //       value2: [{a: 'a'}, {b: 'c'}],
-  //       equal: false
-  //     },
-  //     {
-  //       description: 'pseudo array and equivalent array are not equal',
-  //       value1: {'0': 0, '1': 1, length: 2},
-  //       value2: [0, 1],
-  //       equal: false
-  //     }
-  //   ]
-  // },
-  // {
-  //   description: 'Date objects',
-  //   tests: [
-  //     {
-  //       description: 'equal date objects',
-  //       value1: new Date('2017-06-16T21:36:48.362Z'),
-  //       value2: new Date('2017-06-16T21:36:48.362Z'),
-  //       equal: true
-  //     },
-  //     {
-  //       description: 'not equal date objects',
-  //       value1: new Date('2017-06-16T21:36:48.362Z'),
-  //       value2: new Date('2017-01-01T00:00:00.000Z'),
-  //       equal: false
-  //     },
-  //     {
-  //       description: 'date and string are not equal',
-  //       value1: new Date('2017-06-16T21:36:48.362Z'),
-  //       value2: '2017-06-16T21:36:48.362Z',
-  //       equal: false
-  //     },
-  //     {
-  //       description: 'date and object are not equal',
-  //       value1: new Date('2017-06-16T21:36:48.362Z'),
-  //       value2: {},
-  //       equal: false
-  //     }
-  //   ]
-  // },
-  // {
-  //   description: 'RegExp objects',
-  //   tests: [
-  //     {
-  //       description: 'equal RegExp objects',
-  //       value1: /foo/,
-  //       value2: /foo/,
-  //       equal: true
-  //     },
-  //     {
-  //       description: 'not equal RegExp objects (different pattern)',
-  //       value1: /foo/,
-  //       value2: /bar/,
-  //       equal: false
-  //     },
-  //     {
-  //       description: 'not equal RegExp objects (different flags)',
-  //       value1: /foo/,
-  //       value2: /foo/i,
-  //       equal: false
-  //     },
-  //     {
-  //       description: 'RegExp and string are not equal',
-  //       value1: /foo/,
-  //       value2: 'foo',
-  //       equal: false
-  //     },
-  //     {
-  //       description: 'RegExp and object are not equal',
-  //       value1: /foo/,
-  //       value2: {},
-  //       equal: false
-  //     }
-  //   ]
-  // },
-  // {
-  //   description: 'sample objects',
-  //   tests: [
-  //     {
-  //       description: 'big object',
-  //       value1: {
-  //         prop1: 'value1',
-  //         prop2: 'value2',
-  //         prop3: 'value3',
-  //         prop4: {
-  //           subProp1: 'sub value1',
-  //           subProp2: {
-  //             subSubProp1: 'sub sub value1',
-  //             subSubProp2: [1, 2, {prop2: 1, prop: 2}, 4, 5]
-  //           }
-  //         },
-  //         prop5: 1000,
-  //         prop6: new Date(2016, 2, 10)
-  //       },
-  //       value2: {
-  //         prop5: 1000,
-  //         prop3: 'value3',
-  //         prop1: 'value1',
-  //         prop2: 'value2',
-  //         prop6: new Date('2016/03/10'),
-  //         prop4: {
-  //           subProp2: {
-  //             subSubProp1: 'sub sub value1',
-  //             subSubProp2: [1, 2, {prop2: 1, prop: 2}, 4, 5]
-  //           },
-  //           subProp1: 'sub value1'
-  //         }
-  //       },
-  //       equal: true
-  //     }
-  //   ]
+  {
+    description: 'Typed arrays',
+    tests: [
+      {
+        description: 'two empty arrays of the same class are equal',
+        value1: new Int32Array([]),
+        value2: new Int32Array([]),
+        equal: true
+      },
+      {
+        description: 'two empty arrays of the different class are not equal',
+        value1: new Int32Array([]),
+        value2: new Int16Array([]),
+        equal: false
+      },
+      {
+        description: 'equal arrays',
+        value1: new Int32Array([1, 2, 3]),
+        value2: new Int32Array([1, 2, 3]),
+        equal: true
+      },
+      {
+        description: 'not equal arrays (same items, different class)',
+        value1: new Int32Array([1, 2, 3]),
+        value2: new Int16Array([1, 2, 3]),
+        equal: false
+      },
+      {
+        description: 'not equal arrays (different item)',
+        value1: new Int32Array([1, 2, 3]),
+        value2: new Int32Array([1, 2, 4]),
+        equal: false
+      },
+      {
+        description: 'not equal arrays (different length)',
+        value1: new Int32Array([1, 2, 3]),
+        value2: new Int32Array([1, 2]),
+        equal: false
+      },
+      {
+        description: 'pseudo array and equivalent typed array are not equal',
+        value1: {'0': 1, '1': 2, length: 2},
+        value2: new Int32Array([1, 2]),
+        equal: false
+      }
+    ]
   }
 ];
 
