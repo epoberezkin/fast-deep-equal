f5df217e299c9a1a8e3072f160a381000dd4520a
streamich <streamich@gmail.com>
2019-11-30 19:10:06 +0100

    feat: 🎸 add support for React elements

--- build.js
+++ build.js
@@ -6,5 +6,7 @@ doT.templateSettings.strip = false;
 
 var jst = doT.compile(fs.readFileSync('./src/index.jst', 'utf8'));
 fs.writeFileSync('./index.js', jst({es6: false}));
+fs.writeFileSync('./react.js', jst({es6: false, react: true}));
 try { fs.mkdirSync('./es6'); }  catch(e) {}
 fs.writeFileSync('./es6/index.js', jst({es6: true}));
+fs.writeFileSync('./es6/react.js', jst({es6: true, react: true}));
