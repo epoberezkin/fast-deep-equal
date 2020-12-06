fd6b0f61b0417df57932046ab6894e70c0445019
stereobooster <stereobooster@gmail.com>
2020-12-06 07:07:14 +0100

    Support cyclic objects

--- build.js
+++ build.js
@@ -5,8 +5,8 @@ var doT = require('dot');
 doT.templateSettings.strip = false;
 
 var jst = doT.compile(fs.readFileSync('./src/index.jst', 'utf8'));
-fs.writeFileSync('./index.js', jst({es6: false}));
-fs.writeFileSync('./react.js', jst({es6: false, react: true}));
+fs.writeFileSync('./index.js', jst({es6: false, cycles: false }));
+fs.writeFileSync('./react.js', jst({es6: false, react: true, cycles: true }));
 try { fs.mkdirSync('./es6'); }  catch(e) {}
-fs.writeFileSync('./es6/index.js', jst({es6: true}));
-fs.writeFileSync('./es6/react.js', jst({es6: true, react: true}));
+fs.writeFileSync('./es6/index.js', jst({es6: true, cycles: false }));
+fs.writeFileSync('./es6/react.js', jst({es6: true, react: true, cycles: false }));
