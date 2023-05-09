8bcd54354eb241b64d30acc05f0b031c842f92c5
Mohd Ashraf <57627350+mohdashraf010897@users.noreply.github.com>
2023-05-09 07:26:56 +0530

    feat: support esm build
    courtesy -> https://github.com/epoberezkin/fast-deep-equal/pull/105

--- build.js
+++ build.js
@@ -5,8 +5,15 @@ var doT = require('dot');
 doT.templateSettings.strip = false;
 
 var jst = doT.compile(fs.readFileSync('./src/index.jst', 'utf8'));
-fs.writeFileSync('./index.js', jst({es6: false}));
-fs.writeFileSync('./react.js', jst({es6: false, react: true}));
-try { fs.mkdirSync('./es6'); }  catch(e) {}
-fs.writeFileSync('./es6/index.js', jst({es6: true}));
-fs.writeFileSync('./es6/react.js', jst({es6: true, react: true}));
+fs.writeFileSync('./index.js', jst({ es6: false }));
+fs.writeFileSync('./react.js', jst({ es6: false, react: true }));
+try {
+  fs.mkdirSync('./es6');
+} catch (e) {}
+fs.writeFileSync('./es6/index.js', jst({ es6: true }));
+fs.writeFileSync('./es6/react.js', jst({ es6: true, react: true }));
+try {
+  fs.mkdirSync('./esm');
+} catch (e) {}
+fs.writeFileSync('./esm/index.js', jst({ es6: true, esm: true }));
+fs.writeFileSync('./esm/react.js', jst({ es6: true, esm: true, react: true }));
